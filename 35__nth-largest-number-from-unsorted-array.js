160e2d6d955b59db099cdddd69ccaf866166b980
Anantha Boudmanabhan <ananth1995.july22@gmail.com>
2021-12-16 12:31:42 +0530

    Added solution for day 35 challenge

---
+++ 35__nth-largest-number-from-unsorted-array.js
@@ -0,0 +1,18 @@
+//INSTRUCTIONS:
+/*
+Write a JavaScript function to get nth largest element from an unsorted array.
+Input
+    nthlargest([ 43, 56, 23, 89, 88, 90, 99, 652], 4)
+Output
+    89
+*/
+
+function nthlargest(arr, highest) {
+    // write your code here
+    return arr.sort((a, b) => b - a)[highest - 1];
+}
+
+const arr = [43, 56, 23, 89, 88, 90, 99, 652];
+const highest = 4;
+
+console.log(nthlargest(arr, highest));
